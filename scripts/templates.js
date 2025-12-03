a901552a8451c196fb73d06f893f363e58e3c890
Pascal <Pascal97Franken@Gmail.com>
2025-12-03 22:44:39 +0100

    Add test template

---
+++ scripts/templates.js
@@ -0,0 +1,10 @@
+function test(){
+   return /*html*/`
+        <button class="small_pokemon_card">
+            <h2>Name of pokemon</h2>
+            <p>type1</p>
+            <p>type2</p>
+            <img src="" alt="">
+        </button>
+    `
+}

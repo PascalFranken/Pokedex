6682278aef018defc0323a0492083d39c87dd41b
Pascal <Pascal97Franken@Gmail.com>
2025-12-05 22:31:25 +0100

    Add img and type

--- script.js
+++ script.js
@@ -24,10 +24,18 @@ function renderPokemonCard(){
     for (let index = 0; index < currentPokemon.length; index++) {
         let name = currentPokemon[index].name;
         nameUppercase = name.charAt(0).toUpperCase() + name.slice(1);
+        img = currentPokemon[index].sprites.other.home.front_default;
+        typeOne = currentPokemon[index].types[0].type.name
+        // typeTwo = currentPokemon[index].types[1].type.name
+        
         pokemonCard.innerHTML += getPokemonTemplate(index);
     }    
 }
 
+function dialog(){
+    const pokemonName = document.getElementById('pokemonname');
+    pokemonName = nameUppercase
+}
 
 
 
--- scripts/templates.js
+++ scripts/templates.js
@@ -4,10 +4,10 @@ function getPokemonTemplate(index){
             <h2>${nameUppercase}</h2>
             <div class="type_and_img_direction">
                 <div class="type_direction">
-                    <p>type1</p>
-                    <p>type2</p>
+                    <p>${typeOne}</p>
+                    
                 </div>
-                <img class="img_size" src="./assets/img/header_logo.png" alt="">
+                <img class="img_size" src="${img}" alt="">
             </div>
         </button>
     `
